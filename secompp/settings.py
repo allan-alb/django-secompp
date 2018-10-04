fef54f8d8472138872174ddd9a69c444eeb21fb5
allan-alb <allan@alb09@gmail.com>
2018-10-04 14:21:35 -0300

    Adicionando pythonanywhere aos allowed hosts do settings

--- secompp/settings.py
+++ secompp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'g!&2yj#21y)zlswt_4)2mr)7=cbx@ys+5o3d&az6q0pvls95(a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
